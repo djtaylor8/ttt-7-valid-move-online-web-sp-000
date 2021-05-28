eee5fab51ee8380eba442b5badb9fbf3f4bcb0ec
DJ Taylor <dtaylor88@gmail.com>
2021-05-28 03:58:46 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,7 +1,10 @@
 # code your #valid_move? method here
 def valid_move?(board, index)
-  if index.
-  
+  if index.between?(0, 8) == true && position_taken? == true
+    true
+  else 
+    false
+  end
 end
 
 
